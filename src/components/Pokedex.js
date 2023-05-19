346abe9bc5d8867034028e41e79b332a192bee77
FabioMateus1510 <fabiomateus1510@gmail.com>
2023-05-19 00:41:18 -0300

    .

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -62,11 +62,34 @@ const Pokedex = (props) => {
   return (
     <div className='pokedex'>
       <div className='pokedex-header'>
-        <input
-          className='searchbar'
-          placeholder='Buscar'
-          onChange={onChangeHandler}
-        />
+        <div className='filters'>
+          <input
+            className='searchbar'
+            placeholder='Buscar'
+            onChange={onChangeHandler}
+          />
+          <select class='types-option'>
+            <option value='' selected disabled></option>
+            <option value='normal'>Normal</option>
+            <option value='fire'>Fire</option>
+            <option value='water'>Water</option>
+            <option value='grass'>Grass</option>
+            <option value='electric'>Electric</option>
+            <option value='ice'>Ice</option>
+            <option value='fighting'>Fighting</option>
+            <option value='poison'>Poison</option>
+            <option value='ground'>Ground</option>
+            <option value='flying'>Flying</option>
+            <option value='psychic'>Psychic</option>
+            <option value='bug'>Bug</option>
+            <option value='rock'>Rock</option>
+            <option value='ghost'>Ghost</option>
+            <option value='dragon'>Dragon</option>
+            <option value='dark'>Dark</option>
+            <option value='steel'>Steel</option>
+            <option value='fairy'>Fairy</option>
+          </select>
+        </div>
         <div className='title-container'>
           <h1 className='title'>PokeCards</h1>
         </div>
